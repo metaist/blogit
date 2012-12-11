05d48f69051d633df5d6dbfe1c748135779773d7
The Metaist <metaist@metaist.com>
2012-12-11 11:57:26 -0500

    add: build infrastructure

---
+++ blogit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding: utf-8
+
+"""Static blog generator.
+
+Blogit is static blog generator similar to Jekyll and Hyde.
+"""
+
+__author__ = 'The Metaist'
+__copyright__ = 'Copyright 2012, Metaist'
+__email__ = 'metaist@metaist.com'
+__license__ = 'MIT'
+__maintainer__ = 'The Metaist'
+__status__ = 'Prototype'
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from distutils.core import setup
+
+import blogit
+
+setup(
+    name='blogit',
+    version=blogit.__version__,
+    author=blogit.__author__,
+    author_email=blogit.__email__,
+    url='https://github.com/metaist/blogit',
+    download_url='https://github.com/metaist/blogit',
+    description=blogit.__doc__.split('\n')[0],
+    long_description=blogit.__doc__,
+    py_modules=['blogit'],
+    keywords='static blog generator',
+    license=blogit.__license__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
